b7097007b63cc963d5a865af51f155817b96071b
Kiri Nakata <kiri.nakata@mixi.co.jp>
2016-07-28 06:03:43 +0000

    個体値算出機能を追加

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import re
 import math
 import json
 
+arc_table = [0.0940000,0.1351374,0.1663979,0.1926509,0.2157325,0.2365727,0.2557201,0.2735304,0.2902499,0.3060574,0.3210876,0.3354450,0.3492127,0.3624578,0.3752356,0.3875924,0.3995673,0.4111936,0.4225000,0.4335117,0.4431076,0.4530600,0.4627984,0.4723361,0.4816850,0.4908558,0.4998584,0.5087018,0.5173940,0.5259425,0.5343543,0.5426358,0.5507927,0.5588306,0.5667545,0.5745692,0.5822789,0.5898879,0.5974000,0.6048188,0.6121573,0.6194041,0.6265671,0.6336492,0.6406530,0.6475810,0.6544356,0.6612193,0.6679340,0.6745819,0.6811649,0.6876849,0.6941437,0.7005429,0.7068842,0.7131691,0.7193991,0.7255756,0.7317000,0.7347410,0.7377695,0.7407856,0.7437894,0.7467812,0.7497610,0.7527291,0.7556855,0.7586304,0.7615638,0.7644861,0.7673972,0.7702973,0.7731865,0.7760650,0.7789328,0.7817901,0.7846370,0.7874736,0.7903000,0.7931164];
+
+class IV:
+    def __init__(self, stamina, attack, defense):
+        self.stamina = stamina
+        self.attack = attack
+        self.defense = defense
+
+
 class Pokemon:
     pokemon_table = json.load(open("pokemon.json"))
 
@@ -18,14 +27,30 @@ class Pokemon:
         self.nickname = ""
         self.base_stamina = self.data["BaseStamina"]
         self.base_attack = self.data["BaseAttack"]
-        self.base_defence = self.data["BaseDefense"]
+        self.base_defense = self.data["BaseDefense"]
+        self.iv = IV(0,0,0)
 
-arc_table = [0.0940000,0.1351374,0.1663979,0.1926509,0.2157325,0.2365727,0.2557201,0.2735304,0.2902499,0.3060574,0.3210876,0.3354450,0.3492127,0.3624578,0.3752356,0.3875924,0.3995673,0.4111936,0.4225000,0.4335117,0.4431076,0.4530600,0.4627984,0.4723361,0.4816850,0.4908558,0.4998584,0.5087018,0.5173940,0.5259425,0.5343543,0.5426358,0.5507927,0.5588306,0.5667545,0.5745692,0.5822789,0.5898879,0.5974000,0.6048188,0.6121573,0.6194041,0.6265671,0.6336492,0.6406530,0.6475810,0.6544356,0.6612193,0.6679340,0.6745819,0.6811649,0.6876849,0.6941437,0.7005429,0.7068842,0.7131691,0.7193991,0.7255756,0.7317000,0.7347410,0.7377695,0.7407856,0.7437894,0.7467812,0.7497610,0.7527291,0.7556855,0.7586304,0.7615638,0.7644861,0.7673972,0.7702973,0.7731865,0.7760650,0.7789328,0.7817901,0.7846370,0.7874736,0.7903000,0.7931164];
+    def calc_iv(self):
+        for s in range(0,16):
+            estimated_hp = math.floor((self.base_stamina + s) * arc_table[self.level])
+            if(estimated_hp != self.hp):
+                continue
+            for a in range(0,16):
+               for d in range(0,16):
+                   estimated_cp = math.floor((self.base_attack + a) * math.sqrt(self.base_defense + d) * math.sqrt(self.base_stamina + s) * math.pow(arc_table[self.level], 2) / 10)
+                   if(self.cp == estimated_cp):
+                       self.iv = IV(s,a,d)
+                       print("{0}, {1}, {2}".format(s,a,d))
+
+
+        
+
 
 player_level = 15;
 pokemon_level_max = 1 + player_level * 2
 
 arc_max = arc_table[pokemon_level_max + 1]
+arc_max = arc_table[player_level * 2 - 2]
 
 print("pokemon_level_max=" + str(pokemon_level_max))
 
@@ -34,7 +59,7 @@ def extract_digits(str):
     rep = re.compile(pattern)
     result = rep.search(str)
     if result:
-        return result.group()
+        return int(result.group())
     else:
         return "null"
 
@@ -43,7 +68,7 @@ def extarct_hp(str):
     rep = re.compile(pattern)
     result = rep.search(str.replace(" ", ""))
     if result:
-        return result.group(1)
+        return int(result.group(1))
     else:
         return "null"
 
@@ -55,7 +80,7 @@ def analyze_image(pokemon, path):
         sys.exit(1)
 
     tool = tools[0]
-    img_source = Image.open("image/fuga.png")
+    img_source = Image.open(path)
     img_cp = img_source.crop((218,59,218+201,59+79))
     img_name = img_source.crop((34,507,34+585,507+66))
     img_hp = img_source.crop((198,600,198+267,600+29))
@@ -67,25 +92,38 @@ def analyze_image(pokemon, path):
     radius = (640 - 62*2) / 2
     center = ((640 - 62*2)/2 + 62, 403)
 
-    pokemon_level = 0
+    pokemon_level = 1
 
+
+    draw = ImageDraw.Draw(img_source)
     for i in range(0, pokemon_level_max + 1):
-        theta = arc_table[i] / arc_max * math.pi + 0.02
+        theta = (arc_table[i] - 0.094) / arc_max * math.pi# + 0.02
+        theta = ((arc_table[i]-0.094) * 202.037116 / arc_max) * math.pi / 180# + 0.02
         p = (-math.cos(theta) * radius, -math.sin(theta) * radius)
         pos = tuple([a + b for (a,b) in zip(p, center)])
         pix = img_source.getpixel( pos )
         if max(pix) >= 245:
             pokemon_level = i
+        draw.point(pos, (0x00, 0x00, 0x00))
+        draw.line((pos, center), (0x33,0x33,0x33))
+    img_source.save("image/out.png")
 
     pokemon.cp = extract_digits(res_cp)
     pokemon.hp = extarct_hp(res_hp)
     pokemon.nickname = res_name.replace(" ", "").replace("　", "")
     pokemon.level = pokemon_level
 
-pokemon = Pokemon(1)
-analyze_image(pokemon, "image/fuga.png")
+pokemon = Pokemon(134)
+analyze_image(pokemon, "image/piyo.png")
+pokemon.cp = 1236
+pokemon.hp = 146
 
 print("pokemon_level = " + str(pokemon.level / 2 + 1))
-print("CP => " + pokemon.cp)
-print("NAME => " + pokemon.nickname)
-print("HP => " + pokemon.hp)
+print("CP => " + str(pokemon.cp))
+print("NAME => " + pokemon.name)
+print("NICKNAME => " + pokemon.nickname)
+print("HP => " + str(pokemon.hp))
+
+pokemon.calc_iv()
+
+print("IV: hp=" + str(pokemon.iv.stamina) + " attack=" + str(pokemon.iv.attack) + " defense=" + str(pokemon.iv.defense))
