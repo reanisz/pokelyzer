4e3f41842df19a5ae23a7baa81aa8742722c76a1
Kiri Nakata <kiri.nakata@mixi.co.jp>
2016-07-29 06:07:40 +0000

    マルチスレッド処理で最大8倍の高速化

--- web.py
+++ web.py
@@ -3,18 +3,21 @@ from bottle import route, run, template, static_file, request
 import pdb
 from main import analyze_image, Pokemon
 import uuid
+import concurrent.futures
 
-def analyze(file):
+def analyze(file, pno, plv):
+    print("Analyze Start")
     tmpname = "tmp/" + str(uuid.uuid4()) + ".png"
     file.save(tmpname)
-    poke = Pokemon(int(request.forms.get("pno")))
-    analyze_image(int(request.forms.get("plv")), poke, tmpname)
+    poke = Pokemon(pno)
+    analyze_image(plv, poke, tmpname)
     poke.calc_iv()
     poke.img_path = tmpname + ".out.png"
 
     print("( a,  d,  s)")
     for iv in poke.iv:
         print("({0:>2}, {1:>2}, {2:>2})".format(iv.attack, iv.defense, iv.stamina))
+    print("Analyze Complete")
     return poke
 
 
@@ -25,7 +28,13 @@ def index():
 @route('/upload', method='POST')
 def upload():
     files = list(map(lambda form: form[1], filter(lambda form: form[0] == "img", bottle.request.POST.allitems())))
-    poke = [analyze(f) for f in files]
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=8)
+    pno = int(request.forms.get("pno"))
+    plv = int(request.forms.get("plv"))
+    def co_analyze(file):
+        return analyze(file, pno, plv)
+    poke = list(pool.map(co_analyze, files))
+    #poke = [analyze(f) for f in files]
     return template('index', pokemons=poke)
 
 @route('/tmp/<filepath:path>')
