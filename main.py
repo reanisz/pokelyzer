6ad23acaf046a6b7214dadecb1abc1e2256e69aa
Kiri Nakata <kiri.nakata@mixi.co.jp>
2016-07-29 04:46:25 +0000

    黒塗りにして精度向上

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pyocr.builders
 import re
 import math
 import json
+from itertools import product
 
 arc_table = [0.0940000,0.1351374,0.1663979,0.1926509,0.2157325,0.2365727,0.2557201,0.2735304,0.2902499,0.3060574,0.3210876,0.3354450,0.3492127,0.3624578,0.3752356,0.3875924,0.3995673,0.4111936,0.4225000,0.4335117,0.4431076,0.4530600,0.4627984,0.4723361,0.4816850,0.4908558,0.4998584,0.5087018,0.5173940,0.5259425,0.5343543,0.5426358,0.5507927,0.5588306,0.5667545,0.5745692,0.5822789,0.5898879,0.5974000,0.6048188,0.6121573,0.6194041,0.6265671,0.6336492,0.6406530,0.6475810,0.6544356,0.6612193,0.6679340,0.6745819,0.6811649,0.6876849,0.6941437,0.7005429,0.7068842,0.7131691,0.7193991,0.7255756,0.7317000,0.7347410,0.7377695,0.7407856,0.7437894,0.7467812,0.7497610,0.7527291,0.7556855,0.7586304,0.7615638,0.7644861,0.7673972,0.7702973,0.7731865,0.7760650,0.7789328,0.7817901,0.7846370,0.7874736,0.7903000,0.7931164];
 
@@ -79,6 +80,10 @@ def fitting(t, f, to):
     print(str(res))
     return res
 
+def is_white(pixel):
+    if(pixel[0] + pixel[1] + pixel[2] > 230*3):
+        return 1
+    return 0
 
 def analyze_image(player_level, pokemon, image):
     pokemon_level_max = 1 + player_level * 2
@@ -93,6 +98,12 @@ def analyze_image(player_level, pokemon, image):
 
     tool = tools[0]
     img_source = Image.open(image)
+    draw = ImageDraw.Draw(img_source)
+
+    for x,y in product(*map(range, img_source.size)):
+        if(not is_white(img_source.getpixel((x,y)))):
+            draw.point((x,y), (0x00,0x00,0x00))
+
     img_width = img_source.width
     img_mul = img_source.width / 640
     img_cp   = img_source.crop(fitting((218,59,218+201,59+79), 640, img_width))
@@ -109,16 +120,15 @@ def analyze_image(player_level, pokemon, image):
     pokemon_level = 1
 
 
-    draw = ImageDraw.Draw(img_source)
     for i in range(0, pokemon_level_max + 1):
         theta = ((arc_table[i]-0.094) * 202.037116 / arc_max) * math.pi / 180 + 0.00
         p = (-math.cos(theta) * radius, -math.sin(theta) * radius)
         pos = tuple([a + b for (a,b) in zip(p, center)])
         pix = img_source.getpixel( pos )
-        col = (0x00,0x00,0x00)
+        col = (0x00,0xff,0xff)
         if max(pix) >= 245:
             pokemon_level = i
-            col = (0xff,0x00,0x00)
+            col = (0xff,0xff,0x00)
         draw.point(pos, col)
         draw.line((pos, center), col)
     img_source.save(image + ".out.png")
